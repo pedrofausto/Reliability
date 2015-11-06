78d3d5f85e5a6d893b793580d6e3d6c698b07f38
pedrofausto <pedrofausto@gmail.com>
2015-11-06 01:09:17 -0200

    Updating regression.py  just for showcase

--- regression.py
+++ regression.py
@@ -25,18 +25,30 @@ result_params = lm.params
 print result_params
 
 X_new4 = pd.DataFrame({'Ton': [0.4]})
+X_new5 = pd.DataFrame({'Ton': [0.5]})
 X_new6 = pd.DataFrame({'Ton': [0.6]})
+X_new7 = pd.DataFrame({'Ton': [0.7]})
 X_new8 = pd.DataFrame({'Ton': [0.8]})
 
 
 result4 = lm.predict(X_new4)
+result5 = lm.predict(X_new5)
 result6 = lm.predict(X_new6)
+result7 = lm.predict(X_new7)
 result8 = lm.predict(X_new8)
 print X_new4.head() 
-print result4 
+print result4
+
+print X_new5.head() 
+print result5 
 
 print X_new6.head() 
 print result6
 
+print X_new7.head() 
+print result7 
+
 print X_new8.head() 
 print result8
+
+print data
