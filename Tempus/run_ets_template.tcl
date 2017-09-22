4a1f0d9994ab648a1ca80583d0ea7d82596d900f
Pedro Fausto Rodrigues <pedrofausto@gmail.com>
2017-09-22 12:52:23 -0300

    Updating template. Now the -max_paths option is informed to Tempus

--- Tempus/run_ets_template.tcl
+++ Tempus/run_ets_template.tcl
@@ -3,7 +3,7 @@ read_verilog FILE
 set_top_module TOP
 read_sdc CONSTRAIN
 set_analysis_mode -analysisType single
-report_timing -format {instance cell arc load slew delay arrival required} -nworst NUMBER
+report_timing -format {instance cell arc load slew delay arrival required} -nworst NUMBER –max_paths NUMBER
 report_analysis_coverage
 report_clocks
 update_timing -full
